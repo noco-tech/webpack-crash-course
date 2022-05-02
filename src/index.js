8867a3963bef6f136cb1dcd846734b9c799e7a58
sadainoaraisan <1991000012rg@cyber-u.ac.jp>
2022-05-02 23:56:48 +0900

    implement babel-loader and react development

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
-import _ from 'lodash';
-// import {NAME, Nijou} from './utilities'
-// import {NAME as NAME_OF_HUM} from './utilities'
-// import * as utilities from './utilities'
-// import Tiger from './utilities'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './style.css';
-// import './style.css'
- //styleはこれでもOk省略できる　あれ？できない？やっぱstyleいる
-import './style.scss';
-import logo from './logo.png';
-// console.log(style);
+ReactDOM.render(
+  <div>Hello,React!</div>
 
-// console.log(Tiger.say());
-// console.log(NAME_OF_HUM)
-// console.log(utilities.Nijou(3));
-// console.log(utilities.NAME);
-
-function component() {
-  const element = document.createElement('div');
-  const array = ['Hello', 'webpack', '!!'];
-  element.innerHTML = _.join(array, ' ');
-  return element;
-}
-
-document.body.appendChild(component());
-document.body.classList.add('haikei');
-
-const image = new Image()
-image.src = logo
-document.body.appendChild(image);
+  , document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -10,6 +11,18 @@ module.exports = {
     },
     module: {
       rules: [
+        {
+          test: /\.m?jsx?$/,
+          exclude: /node_modules/,
+          use:{
+
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+          
+        },
         {
           test: /\.css$/,
           use: [
@@ -32,10 +45,20 @@ module.exports = {
             limit: 2048,
             name: './images/[name].[ext]'
           }
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader'
         }
       ],
     },
     devServer: {
       contentBase: outputPath
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+template: './src/index.html',
+filename: './index.html'
+      })
+    ]
 }
