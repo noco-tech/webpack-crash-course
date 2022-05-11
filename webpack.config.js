f11773cb1b9009d2572cde4f5c047f3f0ba7f861
sadainoaraisan <1991000012rg@cyber-u.ac.jp>
2022-05-11 13:39:57 +0900

    install optimize-css-assets-webpack-plugin 改善OK

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
