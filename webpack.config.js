d020d9ead193efaa69899c169bf00113c3d638e8
sadainoaraisan <1991000012rg@cyber-u.ac.jp>
2022-05-11 11:12:26 +0900

    apply uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -59,5 +59,14 @@ filename: './index.html'
       new MiniCssExtractPlugin({
         filename: '[name].[hash].css'
       })
-    ]
+    ],
+    optimization: {
+      minimizer: [new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })],
+    },
 }
